0ec16a5d8af05f20c94bfd10e8ddbac4bf672c9c
Stephane Dorotich <stephanedorotich@gmail.com>
2022-05-16 00:03:33 -0600

    w2_t1

---
+++ w2/fibonacci.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+
+int main(int argc, char ** argv) {
+    // Check that there are 2 arguments
+    if (argc != 2) return -1;
+
+    // Convert argv[1] to an integer n (using a Try-Catch block)
+    int n;
+    try {
+        n = std::stoi(argv[1]);
+    } catch (...) {
+        return -1;
+    }
+
+    // Check that n is greater than 2
+    if (n < 2) return -1;
+
+    // Create a vector containing the first n numbers of the fibonacci sequence
+    std::vector<int> v;
+    v.push_back(0);
+    v.push_back(1);
+
+    for (int i = 2; i < n; i++) {
+        v.push_back(v[i-2] + v[i-1]);
+    }
+
+    // Print all values from the vector
+    for (auto j : v) std::cout << j << " ";
+    std::cout << std::endl;
+
+    return 0;
+}
